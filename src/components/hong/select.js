ff7fe489bff9cc727a6e0952720adb3ba43cf139
Kimhannah <skqkdldhf98@naver.com>
2021-12-08 00:50:43 +0900

    pull main

--- src/components/hong/select.js
+++ src/components/hong/select.js
@@ -1,160 +1,160 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@mui/styles";
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { loginState, qrState } from "../../loginState";
 import { useRecoilState } from "recoil";
 import { useHistory } from "react-router";
 
 const useStyles = makeStyles({
     root: {
-        paddingTop: '50px',
+        paddingTop: "50px",
     },
     grid: {
-        border: '1px solid black',
-        height: '560px'
+        border: "1px solid black",
+        height: "560px",
     },
     header: {
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
     },
     border: {
-        border: '1px sold black',
-        textAlign: 'left',
-        marginBottom: '5px',
+        border: "1px sold black",
+        textAlign: "left",
+        marginBottom: "5px",
     },
     navBar: {
-        backgroundColor: 'black',
-        color: 'white',
-        height: '150px',
-        paddingTop: '20px',
-        marginTop: '10px'
+        backgroundColor: "black",
+        color: "white",
+        height: "150px",
+        paddingTop: "20px",
+        marginTop: "10px",
     },
     select: {
-        paddingTop: '10px',
-        fontSize: '23px',
-    }
-})
+        paddingTop: "10px",
+        fontSize: "23px",
+    },
+});
 
 const Select = (props) => {
     // console.log(props, 'moive component props check')
-    const classes = useStyles()
+    const classes = useStyles();
     const history = useHistory();
-    
+
     const [isLogin, setIsLogin] = useRecoilState(loginState);
     const [qrCheck, setQrCheck] = useRecoilState(qrState);
 
     const [movie, setMovie] = useState({
-        code: '',
-        name: '',
-        age: '',
-    })
+        code: "",
+        name: "",
+        age: "",
+    });
     const [branch, setBranch] = useState({
-        code: '',
-        name: '',
-    })
+        code: "",
+        name: "",
+    });
     const [date, setDate] = useState({
-        code: '',
-        name: ''
-    })
-    const [totalDate, setTotalDate] = useState([])
+        code: "",
+        name: "",
+    });
+    const [totalDate, setTotalDate] = useState([]);
 
     const [time, setTime] = useState({
-        code: '',
-        name: '',
-    })
-    const [totalTime, setTotalTime] = useState([])
+        code: "",
+        name: "",
+    });
+    const [totalTime, setTotalTime] = useState([]);
 
-    let movieNames = []
-    let locations = []
+    let movieNames = [];
+    let locations = [];
 
     if (props.data) {
-        movieNames = props.data.data.movie
-        locations = props.data.data.branch
+        movieNames = props.data.data.movie;
+        locations = props.data.data.branch;
     }
 
     useEffect(() => {
         if (!isLogin.email || !qrCheck.safe) {
-            alert('비정상적인 접근입니다.')
-            history.push("./")
+            alert("로그인 후  이용해주세요.");
+            history.push("./");
         }
         if (movie.code && branch.code && !date.code) {
-            getDate()
+            getDate();
         } else if (movie.code && branch.code && date) {
-            getTime()
-        } 
-    }, [movie, branch, date, time])
+            getTime();
+        }
+    }, [movie, branch, date, time]);
 
     const handleMovie = (event) => {
         setMovie((prevState) => {
-            if (event.target.id.split('/')[1] === '-1') {
+            if (event.target.id.split("/")[1] === "-1") {
                 return {
                     ...prevState,
-                    code: event.target.id.split('/')[0],
+                    code: event.target.id.split("/")[0],
                     name: event.target.textContent,
-                    age: '없음',
-                }
+                    age: "없음",
+                };
             } else {
                 return {
                     ...prevState,
-                    code: event.target.id.split('/')[0],
+                    code: event.target.id.split("/")[0],
                     name: event.target.textContent,
-                    age: event.target.id.split('/')[1],
-                }
+                    age: event.target.id.split("/")[1],
+                };
             }
-        })
-    }
+        });
+    };
     const handleBranch = (event) => {
         setBranch((prevState) => {
             return {
                 ...prevState,
                 code: event.target.id,
-                name: event.target.textContent
-            }
-        })
-    }
+                name: event.target.textContent,
+            };
+        });
+    };
     const handleDate = (event) => {
         setDate((prevState) => {
             return {
                 ...prevState,
                 code: event.target.id,
-                name: event.target.textContent
-            }
-        })
-    }
+                name: event.target.textContent,
+            };
+        });
+    };
     const handleTime = (event) => {
         setTime((prevState) => {
             return {
                 ...prevState,
                 code: event.target.id,
-                name: event.target.textContent
-            }
-        })
-    }
+                name: event.target.textContent,
+            };
+        });
+    };
     const getDate = async () => {
-        const dateInfo = await axios.get('/api/reserve/date', {
+        const dateInfo = await axios.get("/api/reserve/date", {
             params: {
                 movieCode: movie.code,
                 branchCode: branch.code,
-            }
-        })
-        setTotalDate(dateInfo.data)
-    }
+            },
+        });
+        setTotalDate(dateInfo.data);
+    };
     const getTime = async () => {
-        const timeInfo = await axios.get('/api/reserve/time', {
+        const timeInfo = await axios.get("/api/reserve/time", {
             params: {
                 movieCode: movie.code,
                 branchCode: branch.code,
                 theaterDate: date.name,
-            }
-        })
-        setTotalTime(timeInfo.data)
-    }
+            },
+        });
+        setTotalTime(timeInfo.data);
+    };
 
     return (
         <>
@@ -163,14 +163,22 @@ const Select = (props) => {
                 <Grid container>
                     <Grid item xs={3} className={classes.grid}>
                         <div className={classes.header}>영화</div>
-                        <div style={{padding: '10px'}}>
+                        <div style={{ padding: "10px" }}>
                             <div className={classes.border}>
                                 <Button>예매율순</Button>
-                                <Button style={{marginLeft: '15px'}}>가나다순</Button>
+                                <Button style={{ marginLeft: "15px" }}>
+                                    가나다순
+                                </Button>
                             </div>
-                            <div style={{overflow: 'auto', height: '470px'}}>
+                            <div style={{ overflow: "auto", height: "470px" }}>
                                 {movieNames.map((movieName) => (
-                                    <Button key={movieName.M_CODE} id={`${movieName.M_CODE}/${movieName.M_AGE_LIMIT}`}  color="secondary" style={{display: 'block'}} onClick={handleMovie}>
+                                    <Button
+                                        key={movieName.M_CODE}
+                                        id={`${movieName.M_CODE}/${movieName.M_AGE_LIMIT}`}
+                                        color="secondary"
+                                        style={{ display: "block" }}
+                                        onClick={handleMovie}
+                                    >
                                         {movieName.M_NAME}
                                     </Button>
                                 ))}
@@ -179,10 +187,16 @@ const Select = (props) => {
                     </Grid>
                     <Grid item xs={2} className={classes.grid}>
                         <div className={classes.header}>극장</div>
-                        <div style={{padding: '10px'}}>
-                            <div style={{overflow: 'auto', height: '515px'}}>
+                        <div style={{ padding: "10px" }}>
+                            <div style={{ overflow: "auto", height: "515px" }}>
                                 {locations.map((loc) => (
-                                    <Button key={loc.B_CODE} id={loc.B_CODE} color="secondary" style={{display: 'block'}} onClick={handleBranch}>
+                                    <Button
+                                        key={loc.B_CODE}
+                                        id={loc.B_CODE}
+                                        color="secondary"
+                                        style={{ display: "block" }}
+                                        onClick={handleBranch}
+                                    >
                                         {loc.B_NAME}
                                     </Button>
                                 ))}
@@ -191,10 +205,16 @@ const Select = (props) => {
                     </Grid>
                     <Grid item xs={2} className={classes.grid}>
                         <div className={classes.header}>날짜</div>
-                        <div style={{padding: '10px'}}>
-                            <div style={{overflow: 'auto', height: '515px'}}>
+                        <div style={{ padding: "10px" }}>
+                            <div style={{ overflow: "auto", height: "515px" }}>
                                 {totalDate.map((td) => (
-                                    <Button key={td.MT_CODE} id={td.MT_CODE} color="secondary" style={{display: 'block'}} onClick={handleDate}>
+                                    <Button
+                                        key={td.MT_CODE}
+                                        id={td.MT_CODE}
+                                        color="secondary"
+                                        style={{ display: "block" }}
+                                        onClick={handleDate}
+                                    >
                                         {td.MT_RUNNING_DATE}
                                     </Button>
                                 ))}
@@ -203,10 +223,16 @@ const Select = (props) => {
                     </Grid>
                     <Grid item xs={4} className={classes.grid}>
                         <div className={classes.header}>시간</div>
-                        <div style={{padding: '10px'}}>
+                        <div style={{ padding: "10px" }}>
                             {totalTime.map((tt) => (
-                                <Button key={tt.MT_CODE} id={tt.MT_CODE} color="secondary" onClick={handleTime}>
-                                    {tt.MT_SCREEN_SPACE} {tt.MT_START_TIME} ({tt.MT_AVAIL_SEAT} / {tt.MT_TOTAL_SEAT}) 
+                                <Button
+                                    key={tt.MT_CODE}
+                                    id={tt.MT_CODE}
+                                    color="secondary"
+                                    onClick={handleTime}
+                                >
+                                    {tt.MT_SCREEN_SPACE} {tt.MT_START_TIME} (
+                                    {tt.MT_AVAIL_SEAT} / {tt.MT_TOTAL_SEAT})
                                 </Button>
                             ))}
                         </div>
@@ -217,36 +243,73 @@ const Select = (props) => {
                 <Container>
                     <Grid container>
                         <Grid item xs={2}>
-                            <div style={{height: '100px', borderRight: '1px solid white', marginLeft: '20%'}}>
+                            <div
+                                style={{
+                                    height: "100px",
+                                    borderRight: "1px solid white",
+                                    marginLeft: "20%",
+                                }}
+                            >
                                 <div>예매정보</div>
                                 <div>제목 : {movie.name}</div>
                                 <div>나이 제한 : {movie.age}</div>
                             </div>
                         </Grid>
                         <Grid item xs={3}>
-                            <div style={{height: '100px', borderRight: '1px solid white', marginLeft: '20%'}}>
+                            <div
+                                style={{
+                                    height: "100px",
+                                    borderRight: "1px solid white",
+                                    marginLeft: "20%",
+                                }}
+                            >
                                 <div>극장 : {branch.name}</div>
-                                <div>일시 : {date.name} {time.name.split(' ')[2]}</div>
-                                <div>상영관 : {time.name.split(' ')[0]} {time.name.split(' ')[1]}</div>
+                                <div>
+                                    일시 : {date.name} {time.name.split(" ")[2]}
+                                </div>
+                                <div>
+                                    상영관 : {time.name.split(" ")[0]}{" "}
+                                    {time.name.split(" ")[1]}
+                                </div>
                             </div>
                         </Grid>
-                        <Grid item xs={7} style={{color: '#808080'}}>
-                            <div style={{paddingTop: '10px', marginLeft: '5%'}}>
+                        <Grid item xs={7} style={{ color: "#808080" }}>
+                            <div
+                                style={{ paddingTop: "10px", marginLeft: "5%" }}
+                            >
                                 <ArrowForwardIosIcon />
-                                <span className={classes.select}>&nbsp;좌석 선택&nbsp;</span>
+                                <span className={classes.select}>
+                                    &nbsp;좌석 선택&nbsp;
+                                </span>
                                 <ArrowForwardIosIcon />
-                                <span className={classes.select}>&nbsp;결제&nbsp;</span>
-                                <Button variant="contained" color="error" style={{maxWidth: '100px', minWidth: '100px', marginLeft: '35%', borderRadius: 0, minHeight: '100px', maxHeight: '100px'}}>
-                                    <Link style={{textDecoration: 'none'}} to={{
-                                        pathname: "/movie/seat",
-                                        state: {
-                                            movie : movie,
-                                            branch: branch,
-                                            date: date,
-                                            time: time,
-                                            email: isLogin.email
-                                        }
-                                    }}>
+                                <span className={classes.select}>
+                                    &nbsp;결제&nbsp;
+                                </span>
+                                <Button
+                                    variant="contained"
+                                    color="error"
+                                    style={{
+                                        maxWidth: "100px",
+                                        minWidth: "100px",
+                                        marginLeft: "35%",
+                                        borderRadius: 0,
+                                        minHeight: "100px",
+                                        maxHeight: "100px",
+                                    }}
+                                >
+                                    <Link
+                                        style={{ textDecoration: "none" }}
+                                        to={{
+                                            pathname: "/movie/seat",
+                                            state: {
+                                                movie: movie,
+                                                branch: branch,
+                                                date: date,
+                                                time: time,
+                                                email: isLogin.email,
+                                            },
+                                        }}
+                                    >
                                         좌석선택
                                     </Link>
                                 </Button>
@@ -256,7 +319,7 @@ const Select = (props) => {
                 </Container>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Select;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,55 +4,65 @@ import LogInOut from "../components/kim/LogInOut";
 import { useRecoilState } from "recoil";
 import { loginState, qrState } from "../loginState";
 import { useHistory } from "react-router";
-import QR from '../components/lee/check'
- 
+import QR from "../components/lee/check";
+
 const Home = (props) => {
     const [isLogin, setIsLogin] = useRecoilState(loginState);
     const [qrCheck, setQrCheck] = useRecoilState(qrState);
-    console.log(qrCheck, 'QR STATE')
+    console.log(qrCheck, "QR STATE");
     const history = useHistory();
 
     const toMovie = () => {
-        console.log('진입')
+        console.log("진입");
         if (!isLogin) {
-            alert("로그인이 필요합니다.")
+            alert("로그인이 필요합니다.");
         } else if (!qrCheck.safe) {
-            alert("코로나 위험이 있습니다")
+            alert("코로나 위험이 있습니다");
         } else {
             history.push({
-                pathname: '/movie',
+                pathname: "/movie",
                 state: {
-                    email: isLogin.email
-                }
-            })
+                    email: isLogin.email,
+                },
+            });
         }
-    }
+    };
 
     const qrFail = () => {
-        console.log('진입')
+        console.log("진입");
         setQrCheck({
             check: false,
             safe: false,
-        })
-        console.log(qrCheck)
-    }
+        });
+        console.log(qrCheck);
+    };
 
     return (
         <>
             <div>
-                {qrCheck.check? (
+                {qrCheck.check ? (
                     <>
                         <h1>Home page</h1>
                         <li
                             type="button"
                             onClick={toMovie}
-                            style={{display: 'inline-block', fontFamily: 'Thysen', fontWeight: 400, fontSize: '18px', color: '#7b533f', textTransform: 'uppercase', lineHeight: '20px', padding: '8px 20px', letterSpacing: '1px'}}
+                            style={{
+                                display: "inline-block",
+                                fontFamily: "Thysen",
+                                fontWeight: 400,
+                                fontSize: "18px",
+                                color: "#7b533f",
+                                textTransform: "uppercase",
+                                lineHeight: "20px",
+                                padding: "8px 20px",
+                                letterSpacing: "1px",
+                            }}
                         >
                             Reserve
                         </li>
-                        <button onClick={qrFail}>
+                        {/* <button onClick={qrFail}>
                             fail
-                        </button>
+                        </button> */}
                     </>
                 ) : (
                     <QR />
